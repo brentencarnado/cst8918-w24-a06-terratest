1c88b6fe4e4c3ad3cfcb9bee6b32d4eb9c5f235a
Brent <brent.encarnado98@gmail.com>
2024-02-16 20:16:48 -0500

    check for nic and ubuntu version

--- test/azure_webserver_test.go
+++ test/azure_webserver_test.go
@@ -10,7 +10,7 @@ import (
 
 // You normally want to run this under a separate "Testing" subscription
 // For lab purposes you will use your assigned subscription under the Cloud Dev/Ops program tenant
-var subscriptionID string = "<your-azure-subscription-id"
+var subscriptionID string = "3a57a2b6-bb02-44a8-b964-575752c42004"
 
 func TestAzureLinuxVMCreation(t *testing.T) {
 	terraformOptions := &terraform.Options{
@@ -18,7 +18,7 @@ func TestAzureLinuxVMCreation(t *testing.T) {
 		TerraformDir: "../",
 		// Override the default terraform variables
 		Vars: map[string]interface{}{
-			"labelPrefix": "<your-college-id>",
+			"labelPrefix": "enca0002",
 		},
 	}
 
@@ -30,7 +30,20 @@ func TestAzureLinuxVMCreation(t *testing.T) {
 	// Run `terraform output` to get the value of output variable
 	vmName := terraform.Output(t, terraformOptions, "vm_name")
 	resourceGroupName := terraform.Output(t, terraformOptions, "resource_group_name")
-
+	nicName := terraform.Output(t, terraformOptions, "nic_name")
+	imageOffer := terraform.Output(t, terraformOptions, "image_offer")
+	imageSKU := terraform.Output(t, terraformOptions, "image_sku")
+	
 	// Confirm VM exists
 	assert.True(t, azure.VirtualMachineExists(t, vmName, resourceGroupName, subscriptionID))
+
+	//confirm nic exists and is connected to vm
+	nicList := azure.GetVirtualMachineNics(t, vmName, resourceGroupName, subscriptionID)
+	assert.Contains(t, nicList, nicName)
+
+	//confirm vm is running the correct version, values taken fron main.tf
+	correctOffer := "0001-com-ubuntu-server-jammy"
+    correctSKU := "22_04-lts-gen2"
+    assert.Equal(t, correctOffer, imageOffer)
+    assert.Equal(t, correctSKU, imageSKU)
 }
